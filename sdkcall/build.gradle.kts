a2c3f2c901b442230ca19aa5f462399377c8e5ab
rtckitteam <rtckitteam@gmail.com>
2026-06-08 14:19:29 +0700

    Update build.gradle.kts

--- sdkcall/build.gradle.kts
+++ sdkcall/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "cc.neo.sdkcall"
-    version = "1.2.1-rc.8"
+    version = "1.2.1-rc.9"
     compileSdk = 35
 
     defaultConfig {
@@ -79,7 +79,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.rtckitteam"
                 artifactId = "neo-sdk-call"
-                version = "1.2.1-rc.8"
+                version = "1.2.1-rc.9"
                 from(components["release"])
             }
         }
